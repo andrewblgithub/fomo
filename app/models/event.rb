abf03114ed9f980f1ac46486321716f185004095
andreblgithub <andrewbl@icloud.com>
2017-05-12 15:45:59 +0000

    group and event models set up and passed rspec

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :group
+  validates_presence_of :title, :description, :created_by, :expires_at
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  has_many :events, dependent: :destroy
+  validates_presence_of :name, :created_by
 end
